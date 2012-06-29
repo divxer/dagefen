1e457634feeab7fbcaf6d7e305bbf02b402eca9c
divxer <wind.cpp@gmail.com>
2012-06-29 23:46:51 +0800

    实现模糊搜索功能

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import org.apache.commons.lang.StringUtils;
 import play.*;
 import play.mvc.*;
 
@@ -9,10 +10,17 @@ import models.*;
 
 public class Application extends Controller {
 
-    public static void index(int page) {
-        List<Album> albumList = Album.find(
-                "order by id desc"
-        ).fetch(page == 0 ? 1 : page, 12);
+    public static void index(int page, String searchText) {
+        List<Album> albumList;
+        flash.put("searchText", searchText);
+        if (StringUtils.isEmpty(searchText)) {
+            albumList = Album.find(
+                    "order by id desc"
+            ).fetch(page == 0 ? 1 : page, 12);
+        } else {
+            albumList = Album.find("title like ? or description like ?",
+                    "%" + searchText + "%", "%" + searchText + "%").fetch(page == 0 ? 1 : page, 12);
+        }
 
         render(albumList);
     }

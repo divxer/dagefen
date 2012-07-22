1c5706b7576aa8a2e2fe4df6a0431a5f4029020c
divxer <wind.cpp@gmail.com>
2012-07-22 09:14:13 +0800

    加入图片延迟加载

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -14,10 +14,10 @@ public class Application extends Controller {
         if (StringUtils.isEmpty(searchText)) {
             albumList = Album.find(
                     "order by id desc"
-            ).fetch(page == 0 ? 1 : page, 12);
+            ).fetch(page == 0 ? 1 : page, 24);
         } else {
             albumList = Album.find("title like ? or description like ?",
-                    "%" + searchText + "%", "%" + searchText + "%").fetch(page == 0 ? 1 : page, 12);
+                    "%" + searchText + "%", "%" + searchText + "%").fetch(page == 0 ? 1 : page, 24);
         }
 
         render(albumList);
